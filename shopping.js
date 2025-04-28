4b7665e9ce2acb7555b9f20b22ecd54062fd4745
mahnoorhusain <mahnooorhusain@gmail.com>
2025-04-28 21:54:06 +0500

    Add files via upload

---
+++ shopping.js
@@ -0,0 +1,5 @@
+let banner = alert (`welcome to our shop!`);
+let item = prompt (`what are you buying?`);
+let price = parseInt (prompt (`what is the price?`));
+let qt = parseInt (prompt (`what quantity are you buying?`));
+let subtotal = alert (`your subtotal is: ${qt*price}`)
